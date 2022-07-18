3bb529c96bae05249f13630b3fdb02ff0ae89f12
Alaa ELShaher <aelshaher@TradeNET.COM>
2022-07-18 13:01:31 +0200

    edite item

--- src/app/table-component/table-component.component.ts
+++ src/app/table-component/table-component.component.ts
@@ -18,15 +18,36 @@ export class TableComponentComponent implements OnInit {
   });
 
   ngOnInit(): void {}
-
+  currentStudent: Student;
   modalRef?: BsModalRef;
   constructor(private modalService: BsModalService) {}
 
   openModal(template: TemplateRef<any>, item: Student) {
     this.modalRef = this.modalService.show(template);
     console.log('item', item);
+    this.studentForm.setValue({
+      name: item.name,
+      age: item.age,
+      email: item.email,
+    });
+    this.currentStudent = item;
   }
   deleteItem(id: any) {
     this.StudentsArr = this.StudentsArr.filter((item) => item.id != id);
+  }
+
+  onSubmit() {
+    this.StudentsArr = this.StudentsArr.map((p) =>
+      p.id === this.currentStudent.id
+        ? {
+            ...p,
+            name: this.studentForm.value.name,
+            age: this.studentForm.value.age,
+            email: this.studentForm.value.email,
+          }
+        : p
+    );
+
+    this.modalService.hide(1);
   }
 }
